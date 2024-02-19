c3b97d2966ab746650516d6a05f22ba42357959b
Shawn Pearson <1157wcl@gmail.com>
2024-02-19 15:20:48 -0500

    add manifests

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,31 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "SuchCodeWow",
+      logo: {
+        src: "./src/assets/wow.png",
+      },
+      tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 3 },
+      social: {
+        github: "https://github.com/suchcodewow/scwastro",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
