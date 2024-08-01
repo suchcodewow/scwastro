7fd7080df42906ae7e6b56077cf453f3ca47d982
Shawn Pearson <1157wcl@gmail.com>
2024-08-01 08:52:50 -0400

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,41 +1,49 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      favicon: "favicon.ico",
-      title: "suchcodewow",
-      customCss: ["./src/styles.css"],
-      components: {
-        Sidebar: "./src/components/Sidebar.astro",
-      },
-      logo: {
-        src: "./src/img/wow.png",
-      },
-      tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 3 },
-      social: {
-        github: "https://github.com/suchcodewow/scwastro",
-      },
-      sidebar: [
-        {
-          label: "Full Stack Front-to-Back",
-          autogenerate: { directory: "fullstack" },
-        },
-        {
-          label: "Pepper",
-          autogenerate: { directory: "pepper" },
-        },
-        {
-          label: "Scripts",
-          autogenerate: { directory: "scripts" },
-        },
-        {
-          label: "Harness",
-          autogenerate: { directory: "harness" },
-        },
-      ],
-    }),
-  ],
+  integrations: [starlight({
+    favicon: "favicon.ico",
+    title: "suchcodewow",
+    customCss: ["./src/styles.css"],
+    components: {
+      Sidebar: "./src/components/Sidebar.astro"
+    },
+    logo: {
+      src: "./src/img/wow.png"
+    },
+    tableOfContents: {
+      minHeadingLevel: 2,
+      maxHeadingLevel: 3
+    },
+    social: {
+      github: "https://github.com/suchcodewow/scwastro"
+    },
+    sidebar: [{
+      label: "Full Stack Front-to-Back",
+      autogenerate: {
+        directory: "fullstack"
+      }
+    }, {
+      label: "Pepper",
+      autogenerate: {
+        directory: "pepper"
+      }
+    }, {
+      label: "Scripts",
+      autogenerate: {
+        directory: "scripts"
+      }
+    }, {
+      label: "Harness",
+      autogenerate: {
+        directory: "harness"
+      }
+    }]
+  })],
+  output: "server",
+  adapter: vercel()
 });
