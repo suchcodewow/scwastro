a91d3d616732cddbbbd9425bfb6be1f0c78ad3b7
Shawn Pearson <1157wcl@gmail.com>
2025-02-05 16:56:20 -0600

    apipull

--- public/manifests/apipull.ps1
+++ public/manifests/apipull.ps1
@@ -1,26 +1,46 @@
 $apikey = "pat.EeRjnXTnS4GrLG5VNNJZUw.67434db428e64616d95199f5.MpQxajcYPOrxAKtqSt9U"
-# $account = "EeRjnXTnS4GrLG5VNNJZUw"
+$account = $apikey.split(".")[1]
 
-# Retrieve all account connectors
-# reference: https://apidocs.harness.io/tag/Connectors#operation/getConnectorList
-$headers = @{
-    'x-api-key'    = $apikey
-    'Content-Type' = "application/json"
-}
-# https://app.harness.io/ng/api/connectors/listV2?accountIdentifier=EeRjnXTnS4GrLG5VNNJZUw
-# https://app.harness.io/ng/api/connectors?pageSize=1000
-# $Body = @{
-# filterType = "Connector"
-# }
-$response = Invoke-RestMethod -method Post -uri "https://app.harness.io/ng/api/connectors/listV2?pageSize=1000" -Headers $headers 
-$connectorList = $response.data.content
-write-host $connectorList.count
-foreach ($connectorItem in $connectorList) {
-    $connector = $connectorItem.connector
-    if ($connector.name.contains("Arti")) {
-        $connector.name
-        $connector.spec
-        $connector.spec.auth.spec.username
-        $connector.spec.auth.spec.passwordRef
+function Get-Secrets() {
+    $headers = @{'x-api-key' = $apikey
+        'Content-Type'       = "application/json" 
     }
+    $Body = @{"filterType" = "Secret" } | Convertto-Json
+    $response = Invoke-RestMethod -method Post -uri "https://app.harness.io/ng/api/v2/secrets/list/secrets?accountIdentifier=$account&pageSize=1000" -Headers $headers -body $Body
+    return $response.data.content.secret
+
 }
+
+function Get-Connectors() {
+    $headers = @{'x-api-key' = $apikey;'Content-Type' = "application/json" }
+    $Body = @{filterType = "Connector" }
+    $response = Invoke-RestMethod -method Post -uri "https://app.harness.io/ng/api/connectors/listV2?pageSize=1000" -Headers $headers 
+    return $response.data.content.connector
+}
+
+function Get-CommaSeparatedSecrets() {
+    $allSecrets = Get-Secrets
+    # $allSecrets
+    $commaSeparatedSecrets = ""
+    foreach ($secret in $allSecrets) {
+        if ($commaSeparatedSecrets.length -gt 0) {
+            $commaSeparatedSecrets = "$commaSeparatedSecrets,"
+        }
+        $commaSeparatedSecrets = "$($commaSeparatedSecrets)account.$($secret.identifier);$($secret.type)"
+    }
+    return $commaSeparatedSecrets
+    # $env:secrets = $commaSeparatedSecrets
+}
+
+$allConnectors = Get-Connectors
+$allConnectors
+foreach ($connector in $allConnectors) {
+    Switch ($connector.type) {
+        "GcpKms" {
+            write-host "GcpKms"
+        }
+
+    }
+
+}
+# write-host "variable is: $env:ref"
